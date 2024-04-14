4755a5d6a0827c7b31d9ae073dc1b0e9c5574869
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-14 15:52:55 +0200

    Dodanie godzin i uporządkowanie kodu

--- src/main/java/org/example/klinika/ControllerKlinika.java
+++ src/main/java/org/example/klinika/ControllerKlinika.java
@@ -4,9 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ControllerKlinika {
-    // W kontrolerze, należy dodać tę metodę
-    public String getFactory(int hour, int minute) {
-        return String.format("%02d:%02d", hour, minute);
     }
 
 }
