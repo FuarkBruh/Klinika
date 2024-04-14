6f6d83814ef0f14351876f6128b39bcda28e3579
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-14 15:20:06 +0200

    Utworzenie podstawowego UI oraz dodanie komentarzy

--- src/main/java/org/example/klinika/StarterKlinika.java
+++ src/main/java/org/example/klinika/StarterKlinika.java
@@ -12,7 +12,7 @@ public class StarterKlinika extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StarterKlinika.class.getResource("uiKlinika.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Aplikacja Klinika");
         stage.setScene(scene);
         stage.show();
     }
