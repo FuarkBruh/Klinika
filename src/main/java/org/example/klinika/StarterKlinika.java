af79300891e8c11f4cf2a9dc414cc379dc15cb49
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-23 19:39:24 +0200

    Dodanie ikony i koniec projektu

--- src/main/java/org/example/klinika/StarterKlinika.java
+++ src/main/java/org/example/klinika/StarterKlinika.java
@@ -3,6 +3,7 @@ package org.example.klinika;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,12 @@ public class StarterKlinika extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StarterKlinika.class.getResource("uiKlinika.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+
+        // Poprawiona ścieżka do pliku obrazu
+        String imageUrl = StarterKlinika.class.getResource("/org/example/klinika/eskulap.jpg").toExternalForm();
+        stage.getIcons().add(new Image(imageUrl));
+
         stage.setTitle("Aplikacja Klinika");
         stage.setScene(scene);
         stage.show();
